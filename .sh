0c54bd249a8ad1cd110177e4aae419ac4673428f
JactusTheCactus <threateninglyamusing@gmail.com>
2025-12-09 04:07:18 -0800

    Update .sh

--- .sh
+++ .sh
@@ -24,11 +24,13 @@ for i in pages/*
 			then
 				case "${i#*.}" in
 					md)
-						n="$(echo "$i" | perl -pe '
-							s|^pages/(\w+)\.\w+$|$1|g;
-							s|_| |g;
-							s|\b(\w)|\U$1|g;
-						')"
+						n="$(echo "$i" \
+							| perl -pe '
+								s|^pages/(\w+)\.\w+$|$1|g;
+								s|_| |g;
+								s|\b(\w)|\U$1|g;
+							'
+						)"
 						{
 							echo "# ${n^}"
 							cat "$i"
