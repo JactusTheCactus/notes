ad2eaa630c6597ac5d22e51fc31cec5635fcf53e
JactusTheCactus <threateninglyamusing@gmail.com>
2025-12-24 02:06:16 -0800

    Update .sh

--- .sh
+++ .sh
@@ -5,16 +5,17 @@ rm -f "$DOC"
 touch "$DOC"
 exec > "$DOC" 2>& 1
 find pages -empty -delete
-for i in pages/*; do
-	case "${i#*.}" in
-		md)
-			echo "$i" | perl -pe '
-				s|^pages/(\w+)\.\w+$|# $1|g;
-				s|_| |g;
-				s|\b(\w)(\w*)\b|\u$1\L$2|g;
-			'
-			cat "$i" | perl -pe 's|^#|##|g'
-			echo
-		;;
-	esac
+for i in pages/*
+	do
+		echo "$i" | perl -pe '
+			s|^pages/(\w+)\.\w+$|# $1|g;
+			s|_| |g;
+			s|\b(\w)(\w*)\b|\u$1\L$2|g;
+		'
+		case "${i#*.}" in
+			md)
+				cat "$i" | perl -pe 's|^#|##|g'
+			;;
+		esac
+		echo
 done
