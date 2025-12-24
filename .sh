9d8c474d1322a8f51ae85283d8a1cd96f9729d52
JactusTheCactus <threateninglyamusing@gmail.com>
2025-12-24 02:08:37 -0800

    Update .sh

--- .sh
+++ .sh
@@ -7,15 +7,14 @@ exec > "$DOC" 2>& 1
 find pages -empty -delete
 for i in pages/*
 	do
-		echo "$i" | perl -pe '
-			s|^pages/(\w+)\.\w+$|# $1|g;
+		printf '# %s\n' "$(echo "$i" | perl -pe '
+			s|^pages/(\w+)\.\w+$|$1|g;
 			s|_| |g;
 			s|\b(\w)(\w*)\b|\u$1\L$2|g;
-		'
+		')"
 		case "${i#*.}" in
 			md)
-				cat "$i" | perl -pe 's|^#|##|g'
+				printf '%s\n' "$(cat "$i" | perl -pe 's|^#|##|g')"
 			;;
 		esac
-		echo
 done
